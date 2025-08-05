e4fcd370a8acac0f13619d861d4e302356f30dce
zainabkhan9118 <zainabkhan3473@gmail.com>
2025-08-05 15:02:59 +0500

    made changes to routes and configurations

---
+++ src/hooks/use-mobile.ts
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+
+  React.useEffect(() => {
+    const mql = window.matchMedia("(max-width: 768px)")
+    const onChange = () => {
+      setIsMobile(window.innerWidth < 768)
+    }
+    mql.addEventListener("change", onChange)
+    setIsMobile(window.innerWidth < 768)
+    return () => mql.removeEventListener("change", onChange)
+  }, [])
+
+  return !!isMobile
+}
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -18,12 +18,9 @@ function Router() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/privacy-policy" element={<Policy />} />
-
       </Routes>
     </Layout>
   )
 }
 
 export default Router
-
-
